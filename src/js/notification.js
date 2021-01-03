dab5498828b2454a6aab508688f5f001cb026543
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2021-01-03 23:58:45 +0200

    adds placeholder

--- src/js/notification.js
+++ src/js/notification.js
@@ -1,4 +1,4 @@
-import { error, success, notice } from '@pnotify/core';
+import { error, success } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
